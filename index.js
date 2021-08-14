a8b5f672fa8d69e150a2568abe673fc0af2f9d52
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-08-14 11:29:39 +0530

    Trying to do async operation using async await

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-//-------- Trying to do async operation using promises
+//-------- Trying to do async operation using async await
 
-console.log("Before")
+console.log("Before");
 
-const getUserData = getUser(1);
-getUserData.then((data)=> getRepositories(data))
-   .then((data)=> getCommits(data))
-   .then((data)=> console.log(data))
 
-console.log("After")
+(async function () {
+    const userGet = await getUser(1);
+    const userRepo = await getRepositories(userGet);
+    const userCommit = await getCommits(userRepo);
+    console.log(userCommit);
+  }
+)();
 
-function getUser(id) {  
-    return new Promise(resolve =>{
-      setTimeout(() => {
-        console.log("Reading a user from database...",id);
-        resolve({id: id, name: "Joe",})
-      }, 2000);
-    }) 
-};
 
-function getRepositories(data){
-    return new Promise(resolve => {
-      setTimeout(() => {
-      console.log("Calling GitHub API...",data);
-      resolve({...data,repo :["repo1", "repo2", "repo3"]})
-    }, 2000);})
-};
+console.log("After");
 
-function getCommits (repo){
-    return new Promise(resolve =>{
-      setTimeout(() => {
-        console.log("GitHub Repositories...",repo);
-        resolve({...repo,commit:["commit"]})
-      }, 2000);
-    })
-};
+function getUser(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Reading a user from database...", id);
+      resolve({ id: id, name: "Joe" });
+    }, 2000);
+  });
+}
 
+function getRepositories(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...", data);
+      resolve({ ...data, repo: ["repo1", "repo2", "repo3"] });
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("GitHub Repositories...", repo);
+      resolve({ ...repo, commit: ["commit"] });
+    }, 2000);
+  });
+}
 
 /* 
 
 conclusion : 
 - working
-- promise
+- async await
 - can work with 3rd party async operation
 - return Promise Object
+- Use Semi-colon for the statement that used before IIFE
+X console.log()(function(){}) // cause error
 
  */
